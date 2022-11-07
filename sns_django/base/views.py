0378941f842a4f7428d9aa3728978daa5d347b21
rkawk1234 <okus6977@gmail.com>
2022-11-07 11:50:32 -0500

    post class added

--- sns_django/base/views.py
+++ sns_django/base/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from base.models import Profile
-# from base.models import Post
+from base.models import Post
 
 
 # Create your views here.
@@ -43,7 +43,18 @@ def settings(request):
 
 @login_required(login_url='signin')
 def upload(request):
-    return HttpResponse('<h1>Upload View</h1>')
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+
+        return redirect('/')
+        
+    else:
+        return redirect('/')
 
 
 def signup(request):
