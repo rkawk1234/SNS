90016dcc7fd5ac055105e8775b6e82fb3dbfe88e
rkawk1234 <okus6977@gmail.com>
2022-11-06 20:54:09 -0500

    post class setup
    user profile picture renders fine

--- sns_django/base/views.py
+++ sns_django/base/views.py
@@ -4,13 +4,17 @@ from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from base.models import Profile
+# from base.models import Post
+
 
 # Create your views here.
 
 #it sends user to the signin page when not logged in, django feature
 @login_required(login_url='signin')
 def index(request):
-    return render(request,'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request,'index.html', {'user_profile': user_profile})
 
 @login_required(login_url='signin')
 def settings(request):
@@ -35,6 +39,11 @@ def settings(request):
         return redirect('settings')
   
     return render(request,'setting.html', {'user_profile': user_profile })
+
+
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
 
 
 def signup(request):
@@ -100,5 +109,6 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
 
 
